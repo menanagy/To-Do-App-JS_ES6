32fea87fc2bbbfa90cc773f5b8a63ef43648f810
minanagy <minanagy1993@gmail.com>
2021-03-26 17:53:14 +0200

    Delet Item and Add Clear all Items

--- js/script.js
+++ js/script.js
@@ -4,24 +4,20 @@
 // put all data in bject [FirstName,LastName,Age,UniqueID]
 // put object in Array 
 // store Data Loca
+let count=0;
 
-
-//get Form
+//get Form  
 const student_data=document.getElementById("student");
-//get Form
-
+//get First-Name Input 
 const first_name=document.getElementById("first-name");
-//get Last Name
-
+//get Last Name Input 
 const last_name=document.getElementById("last-name");
-//get Form
-
+//get Age Input 
 const age=document.getElementById("age");
+//get Body of Table
+let students_table=document.querySelector("#students tbody");
 
-const students_table=document.querySelector("#students tbody");
-//Declar Array 
-
-
+//Declare Array 
 if(localStorage.getItem("students")){
     studentArray=JSON.parse(localStorage.getItem("students"));
     console.log(studentArray);
@@ -35,36 +31,38 @@ displayItems(studentArray);
 
 // take Data When Sumbit
 student_data.onsubmit=(e)=>{
-    // take Data When Sumbit
-    e.preventDefault();
-    // console.log(`First Name = ${first_name.value}`);
-    // console.log(`Last Name = ${last_name.value}`);
-    // console.log(`Age = ${age.value}`);
-
-   // put all data in bject [FirstName,LastName,Age,UniqueID]
-   let uid=Math.floor(Math.random()*50000);
-   let student ={uid:uid,first_name:first_name.value,last_name:last_name.value,age:age.value}
-   //console.log(student);
-
-            console.log(`student = ${student}`);
-            studentArray.push(student);
-            displayStudent(student);
-            //Store Object in Array 
-            
-            //console.log(studentArray);
-            // store Data Local Storage
-            localStorage.setItem("students",JSON.stringify(studentArray)); 
-   clearInputs();
+        // take Data When Sumbit
+        e.preventDefault();
+        // console.log(`First Name = ${first_name.value}`);
+        // console.log(`Last Name = ${last_name.value}`);
+        // console.log(`Age = ${age.value}`);
+        let uid=Math.floor(Math.random()*50000);//==>Make unique Id 
+        // put all data in bject [FirstName,LastName,Age,UniqueID]
+        let student ={uid:uid,first_name:first_name.value,last_name:last_name.value,age:age.value};
+        console.log(`student object = ${student}`);//print Object 
+        //Store Object in Array 
+        studentArray.push(student);
+        console.log(`Store  data in  Array  ==> Array = `);
+        console.log(studentArray);
+        // store Data Local Storage
+        localStorage.setItem("students",JSON.stringify(studentArray));
+        console.log("Store Array data in  localstorage = ");
+        console.log(JSON.parse(localStorage.getItem("students"))); 
+        displayStudent(student);
+        
+        clearInputs();
  
 }
 
 displayStudent=(student)=>{
-        
-                students_table.innerHTML+=`<tr class="text-center text-white">
-                <td>${student.first_name}</td>
-                <td>${student.last_name}</td>
-                <td>${student.age} </td>
-                <td><i class="fa fa-trash btn btn-danger" onClick="delet_item(${student.uid})" id="${student.uid}"></i></td>`
+                count++;
+                console.log(`Welcome Count =  ${count}`);
+                displayStudentHTML(student);
+                data=JSON.parse(localStorage.getItem("students"))   
+                console.log(`dataLength= ${data.length}`); 
+                if(data.length==1){
+                    displayTfoot();
+                }
 
 }
 
@@ -76,25 +74,12 @@ clearInputs=()=>{
 }
 //Display all Items From Local Storage 
 function displayItems (arr){
-    console.log(arr);
+    console.log(`displayItems : Array Local Storage  =  ${arr} `);
     arr.map((student)=>{
-         students_table.innerHTML+=`<tr class="text-center text-white">
-        <td>${student.first_name}</td>
-        <td>${student.last_name}</td>
-        <td>${student.age} </td>
-        <td><i class="fa fa-trash btn btn-danger" onClick="delet_item(${student.uid})" id="${student.uid}"></i></td>`
+        displayStudentHTML(student);
     });
    if(arr.length>0){
-        document.querySelector("#students").innerHTML+=`
-        <tfoot>
-            <tr>
-                <td colspan="4">
-                     <button class="btn btn-danger btn-block w-100" onClick="ClearAll()">Clear Items</button>
-                    </td>
-                </tr>
-           </tfoot>
-        `
-        
+        displayTfoot();
     }
    
 }
@@ -103,18 +88,59 @@ function delet_item(id){
     console.log(id);
     console.log(document.getElementById(id));
     //tr--> td --> Delet icon (i want Delete Tr)
-    console.log(document.getElementById(id).parentNode);
-    console.log(document.getElementById(id).parentNode.parentNode);
+    //console.log(document.getElementById(id).parentNode);//==>td
+    //console.log(document.getElementById(id).parentNode.parentNode);//==>tr
     document.getElementById(id).parentNode.parentNode.remove();
     data=JSON.parse(localStorage.getItem("students"))
     newData=data.filter((item)=>item.uid !== id);//All Data Without Specific id 
     console.log(newData);
-    localStorage.setItem("students",JSON.stringify(newData)); 
+    if(newData.length>0){
+        localStorage.setItem("students",JSON.stringify(newData)); 
 
+    }
+    else{
+        ClearAll();
+        //localStorage.setItem("students","[]");
+        //document.querySelector("#students tbody").innerHTML='';
+
+    }
 }
 ClearAll = () =>{
     console.log("Clear ALL");
     localStorage.setItem("students","[]");
+    let students_table=document.querySelector("#students tbody");
     students_table.innerHTML='';
-    document.querySelector("#students tfoot").remove();
+   document.querySelector("#students tfoot").remove();
+   data=JSON.parse(localStorage.getItem("students"))   
+   console.log(`After Clear All ==> dataLength= ${data.length}`); 
+
+}
+
+function displayTfoot(){
+    console.log("Start display Tfoot");
+    document.querySelector("#students").innerHTML+=`
+        <tfoot>
+            <tr>
+                <td colspan="4">
+                     <button class="btn btn-danger btn-block w-100" onClick="ClearAll()">Clear Items</button>
+                    </td>
+                </tr>
+           </tfoot>
+        `
+        console.log("Finish display Tfoot");
+}
+
+function displayStudentHTML(student){
+    console.log("Start displayStudentHTML ");
+    let students_table=document.querySelector("#students tbody");
+    students_table.innerHTML += `
+    <tr class="text-center text-white">
+        <td>${student.first_name}</td>
+        <td>${student.last_name}</td>
+        <td>${student.age} </td>
+        <td><i class="fa fa-trash btn btn-danger" onClick="delet_item(${student.uid})" id="${student.uid}"></i></td>
+    </tr>
+    `
+    console.log("Finish displayStudentHTML");
+
 }
